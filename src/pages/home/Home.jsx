8461376f9c0e5190511e1709e855b7a67f06fa62
mujtaba_Hussain <mujtaba51472@gmail.com>
2022-08-22 03:17:23 +0500

    created the project

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../home/home.scss'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <h1 className='title'>Mujtaba</h1>
+    </div>
+  )
+}
+
+export default Home
